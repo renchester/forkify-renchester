7e2b7097d6b7d7550894f35f81d2e4b81437e16d
renchester <renchesterjramos@gmail.com>
2021-06-14 11:54:49 +0800

    Modified configuration

--- src/js/config.js
+++ src/js/config.js
@@ -6,6 +6,6 @@ export const TIMEOUT_SEC = 10;
 
 export const RES_PER_PAGE = 10;
 
-export const KEY = '80b32f7c-3e58-401f-ada8-9141ad03fd05';
+export const KEY = '620364bb-2dd6-4142-90f8-d9d8504d27a6';
 
 export const MODAL_CLOSE_SEC = 2.5;
